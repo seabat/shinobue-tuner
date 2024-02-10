ab074756e4e7fd6103919c6dccd5f54a903ad373
seabat <ryouta.nakasnishi.86@gmail.com>
2024-02-10 21:32:47 +0900

    TarsosDSP パッケージの依存を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+
+    // TarsosDSP
+    implementation ("be.tarsos.dsp:core:2.5")
+    implementation ("be.tarsos.dsp:jvm:2.5")
+
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
